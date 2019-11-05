d073e522cd5b26da94531c2c30d8bfb44eb04be7
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-05 22:00:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
-#write your code here
+
 def countdown(seconds)
   number=10
    while number > 0
    puts "#{number} SECOND(S)!"
-    x -= 1
+    number -= 1
     end
   return "HAPPY NEW YEAR!"
 end
